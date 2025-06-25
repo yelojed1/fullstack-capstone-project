072286e3dee524bff962e4e745ef615616bd324d
Julien de Tourris <liberal.it.nz@gmail.com>
2025-06-25 06:18:47 -0400

    lab2 update app.js, giftRoutes.js

--- giftlink-backend/routes/giftRoutes.js
+++ giftlink-backend/routes/giftRoutes.js
@@ -1,61 +1,40 @@
-require('dotenv').config();
-const MongoClient = require('mongodb').MongoClient;
-let url = `${process.env.MONGO_URL}`;
-const client = new MongoClient(url);
+const express = require('express');
+const router = express.Router();
+const connectToDatabase = require('../models/db');
+const logger = require('../logger');
 
-async function connectToDatabase() {
+// Get all gifts
+router.get('/', async (req, res, next) => {
+    logger.info('/ called');
     try {
-        await client.connect();
-        return client.db("giftdb");
-    } catch (e) {
-        console.error('Connection error:', e);
-        throw e;
-    }
-}
-
-router.get('/', async (req, res) => {
-    try {
-        // Task 1: Connect to MongoDB and store connection to db constant
         const db = await connectToDatabase();
 
-        // Task 2: use the collection() method to retrieve the gift collection
         const collection = db.collection("gifts");
-
-        // Task 3: Fetch all gifts using the collection.find method. Chain with toArray method to convert to JSON array
         const gifts = await collection.find({}).toArray();
-
-        // Task 4: return the gifts using the res.json method
-        return res.json(gifts);
+        res.json(gifts);
     } catch (e) {
-        console.error('Error fetching gifts:', e);
-        res.status(500).send('Error fetching gifts');
+        logger.console.error('oops something went wrong', e)
+        next(e);
     }
 });
 
-router.get('/:id', async (req, res) => {
+// Get a single gift by ID
+router.get('/:id', async (req, res, next) => {
     try {
-        // Task 1: Connect to MongoDB and store connection to db constant
         const db = await connectToDatabase();
-
-        // Task 2: use the collection() method to retrieve the gift collection
         const collection = db.collection("gifts");
-
         const id = req.params.id;
-
-        // Task 3: Find a specific gift by ID using the collection.fineOne method and store in constant called gift
         const gift = await collection.findOne({ id: id });
 
         if (!gift) {
-            return res.status(404).send('Gift not found');
+            return res.status(404).send("Gift not found");
         }
 
         res.json(gift);
     } catch (e) {
-        console.error('Error fetching gift:', e);
-        res.status(500).send('Error fetching gift');
+        next(e);
     }
 });
-
 
 
 // Add a new gift
