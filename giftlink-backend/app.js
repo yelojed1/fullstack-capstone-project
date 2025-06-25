c04e08b4681e69565d48b869d77187bb3fd6811c
Julien de Tourris <liberal.it.nz@gmail.com>
2025-06-25 06:22:01 -0400

    Create API for Search Service

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -22,25 +22,16 @@ connectToDatabase().then(() => {
 app.use(express.json());
 
 // Route files
-// Gift API Task 1: import the giftRoutes and store in a constant called giftroutes
 const giftRoutes = require('./routes/giftRoutes');
-
-// Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
-//const searchRoutes = require('./routes/searchRoutes');
-
-
+const searchRoutes = require('./routes/searchRoutes');
 const pinoHttp = require('pino-http');
 const logger = require('./logger');
 
 app.use(pinoHttp({ logger }));
 
 // Use Routes
-// Gift API Task 2: add the giftRoutes to the server by using the app.use() method.
 app.use('/api/gifts', giftRoutes);
-
-// Search API Task 2: add the searchRoutes to the server by using the app.use() method.
-//app.use('/api/search', searchRoutes);
-
+app.use('/api/search', searchRoutes);
 
 // Global Error Handler
 app.use((err, req, res, next) => {
