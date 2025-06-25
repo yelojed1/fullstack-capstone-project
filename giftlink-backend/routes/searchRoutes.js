cf2cc4d7cbf0e0ad123901e13ffa0272345b4ecb
Julien de Tourris <liberal.it.nz@gmail.com>
2025-06-25 08:14:33 -0400

    Develop Back-end Integration for Registration

--- giftlink-backend/routes/searchRoutes.js
+++ giftlink-backend/routes/searchRoutes.js
@@ -1,39 +1,39 @@
-    const express = require('express');
-    const router = express.Router();
-    const connectToDatabase = require('../models/db');
+const express = require('express');
+const router = express.Router();
+const connectToDatabase = require('../models/db');
 
-    // Search for gifts
-    router.get('/', async (req, res, next) => {
-        try {
+// Search for gifts
+router.get('/', async (req, res, next) => {
+    try {
 
-            // Task 1: Connect to MongoDB
-            const db = await connectToDatabase();
-            const collection = db.collection("gifts");
-            // Initialize the query object
-            let query = {};
+        // Task 1: Connect to MongoDB
+        const db = await connectToDatabase();
+        const collection = db.collection("gifts");
+        // Initialize the query object
+        let query = {};
 
-            // Task 2: check if the name exists and is not empty
-            if (req.query.name && req.query.name.trim() !== '') {
-                query.name = { $regex: req.query.name, $options: "i" }; // Using regex for partial match, case-insensitive
-            }
-
-            // Task 3: Add other filters to the query
-            if (req.query.category) {
-                query.category = req.query.category;
-            }
-            if (req.query.condition) {
-                query.condition = req.query.condition;
-            }
-            if (req.query.age_years) {
-                query.age_years = { $lte: parseInt(req.query.age_years) };
-            }
-
-            // Task 4: Fetch filtered gifts
-            const gifts = await collection.find(query).toArray();
-            res.json(gifts);
-        } catch (e) {
-            next(e);
+        // Task 2: check if the name exists and is not empty
+        if (req.query.name && req.query.name.trim() !== '') {
+            query.name = { $regex: req.query.name, $options: "i" }; // Using regex for partial match, case-insensitive
         }
-    });
 
-    module.exports = router;
+        // Task 3: Add other filters to the query
+        if (req.query.category) {
+            query.category = req.query.category;
+        }
+        if (req.query.condition) {
+            query.condition = req.query.condition;
+        }
+        if (req.query.age_years) {
+            query.age_years = { $lte: parseInt(req.query.age_years) };
+        }
+
+        // Task 4: Fetch filtered gifts
+        const gifts = await collection.find(query).toArray();
+        res.json(gifts);
+    } catch (e) {
+        next(e);
+    }
+});
+
+module.exports = router;
